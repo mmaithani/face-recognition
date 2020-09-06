1e36d62a9306a2433077da32f17427ad97ab2f33
Mohit Maithani <mohitmaithani81@gmail.com>
2020-09-06 10:33:49 +0530

    uploading settings and archs

---
+++ urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.conf import settings
+from django.views.static import serve
+from face import views
+from django.conf.urls.static import static
+urlpatterns = [
+                  url(r'^$', views.home),
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^clear', views.clear),
+                  url(r'detect/',views.detect),
+                  url(r'extract/', views.extract),
+                  url(r'train/',views.train),
+                  
+                  # Url to select a file for the predictions
+                  # url('fileselect/', SelectPredFileView.as_view(), name='file_select'),
+                  #(? url(r'^delete/$', FileDeleteView.as_view(), name='APIdelete'),)
+                    
+                 # Url to list all the files in the server
+                  # url('files_list/', FilesList.as_view(), name='files_list'),
+                
+                  url(r'deleteuser/', views.deleteuser),
+                  url(r'detectlive/', views.detectlive),
+                  url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
